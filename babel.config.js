141221c5c221d0b6ad668286c2fb8b818955707d
Sarah Heacock <seheacock@bellsouth.net>
2019-04-11 06:50:08 -0400

    first commit

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+//   ignore: ['node_modules'],
+};
+
+// module.exports = {
+//     presets: ["react-native", "@babel/env", "@babel/preset-typescript"],
+//     plugins: [
+//         "@babel/proposal-class-properties",
+//         "@babel/proposal-object-rest-spread"
+//     ],
+//     ignore: ['node_modules']
+// };
