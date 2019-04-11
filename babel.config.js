1ff980cfb5d733b32d4b24a42932be457e63f6b5
Sarah Heacock <seheacock@bellsouth.net>
2019-04-11 07:10:08 -0400

    cleaned up babel config

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-//   ignore: ['node_modules'],
+    presets: ["module:metro-react-native-babel-preset"],
 };
 
-// module.exports = {
-//     presets: ["react-native", "@babel/env", "@babel/preset-typescript"],
-//     plugins: [
-//         "@babel/proposal-class-properties",
-//         "@babel/proposal-object-rest-spread"
-//     ],
-//     ignore: ['node_modules']
-// };
